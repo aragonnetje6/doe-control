dcae5302fa30ec9c6f6b87608b5bb29612076f17
Grace Stok <gracestok6@gmail.com>
2025-07-02 04:44:03 +0200

    implemented greet2 route

--- doe-control-server/src/main.rs
+++ doe-control-server/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, web};
+use serde::Deserialize;
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
@@ -6,10 +7,21 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[derive(Deserialize)]
+struct Name {
+    name: String,
+}
+
+#[get("/greet2/")]
+async fn greet2(name: web::Query<Name>) -> String {
+    tracing::info!("greeting {}", name.name);
+    format!("Hello from Miss Grace' server, {}!", name.name)
+}
+
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+async fn main() -> ShuttleActixWeb<impl FnOnce(&mut web::ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut web::ServiceConfig| {
+        cfg.service(hello_world).service(greet2);
     };
 
     Ok(config.into())
