25872ff254b676c3db3c7118bc1b3a8413cbf0b6
Grace Stok <gracestok@proton.me>
2025-07-05 04:26:10 +0200

    sql typos

--- doe-control-server/migrations/20250704055251_initial.sql
+++ doe-control-server/migrations/20250704055251_initial.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
-CREATE TABLE Commands {
+CREATE TABLE Commands (
     time TIMESTAMP PRIMARY KEY,
     command TEXT NOT NULL, 
-    signature BYTEA NOT NULL,
-}
+    signature BYTEA NOT NULL
+);
